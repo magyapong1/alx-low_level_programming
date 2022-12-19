08feeed210bc41c35316358b24b1bebf3931b2a6
Michael Agyapong <magyapong021@st.ug.edu.gh>
2022-12-19 17:12:53 +0000

    Updated a file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * main - will print _putchar, followed by a new line
-  * Return: 0 always when successful
-  */
+   * main - will print _putchar, followed by a new line
+    * Return: 0 always when successful
+     */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	
-	return (0);
+	int _putchar(char c)
+	{
+		_putchar('_');
+			_putchar('p');
+				_putchar('u');
+					_putchar('t');
+						_putchar('c');
+							_putchar('h');
+								_putchar('a');
+									_putchar('r');
+										_putchar('\n');
+	}
+
+											return (0);
 }
