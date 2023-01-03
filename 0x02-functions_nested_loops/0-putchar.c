1924d021b2f89d1fcb803fd99f990baa453c99d3
Michael Agyapong <magyapong021@st.ug.edu.gh>
2023-01-03 22:51:25 +0000

    Made some changes to 0x02-functions_nested_loops directory

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-   * main - will print _putchar, followed by a new line
-    * Return: 0 always when successful
-     */
+  * main - entry point
+  * Return: 0 always
+  */
 int main(void)
 {
-	int _putchar(char c)
-	{
-		_putchar('_');
-			_putchar('p');
-				_putchar('u');
-					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
-	}
-
-											return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
